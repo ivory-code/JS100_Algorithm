f24085bcd34477145dc55ee36ef3a19aee99a38e
ivory-code <kyo3553@gmail.com>
2021-11-17 16:26:00 +0900

    Add: solving process of 26

--- JS100/026.js
+++ JS100/026.js
@@ -9,6 +9,7 @@
 */
 
 // 1
+// planets라는 객체안에 key와 value 값을 부여한 후 console로 나타낸다.
 const planets = {
 	'수성' : 'Mercury',
 	'금성' : 'Venus',
@@ -27,6 +28,8 @@ const solution = (name) => {
 solution("수성");
 
 // 2
+// 두 개의 배열에 각각 한글이름과 영어이름 값을 넣어준다.
+// indexOf()를 활용하여 한글이름의 배열 index값을 알아내고 그 값을 곧바로 영어이름 배열의 index값으로 활용한다.
 const planetsArrKor =['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성'];
 const planetsArrUs =['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
 
