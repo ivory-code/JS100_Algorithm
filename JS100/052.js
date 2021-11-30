39aa2aadb332876952e90a8c088b387efd58bbfb
ivory-code <kyo3553@gmail.com>
2021-11-30 15:21:41 +0900

    Add: 51 ~ 52

---
+++ JS100/052.js
@@ -0,0 +1,54 @@
+/*
+문제52 : quick sort
+
+다음 빈 칸을 채워 퀵 정렬을 완성해주세요.
+
+*/
+
+// function quickSort(arr){
+//   if (arr.length <= 1){
+//     return arr;
+//   }
+
+//   const pivot = arr[0];
+//   const left = [];
+//   const right = [];
+
+//   for (let i=1; i<arr.length; i++){
+//     if(/*빈칸을 채워주세요*/){
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+//   return /*빈칸을 채워주세요*/
+// }
+
+// const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+
+// console.log(quickSort(array));
+
+// 1.
+const input = "3 5 88 0";
+const array = input.split(" ").map((n) => parseInt(n, 10));
+
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+};
+
+console.log(quickSort(array));
